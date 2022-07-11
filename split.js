6c7cec33f3db4925aa016a05277803563f8717be
shra14600 <104187381+shra14600@users.noreply.github.com>
2022-07-11 20:07:07 +0530

    split website

---
+++ split.js
@@ -0,0 +1,11 @@
+var left =document.querySelector(".left");
+var right =document.querySelector(".right");
+var container =document.querySelector(".container");
+left.addEventListener("mouseenter", ()=>                    {container.classList.add("hover-left");
+})
+left.addEventListener("mouseleave", ()=>                    {container.classList.add("hover-left");
+})
+right.addEventListener("mouseenter", ()=>                    {container.classList.add("hover-right");
+})
+right.addEventListener("mouseleave", ()=>                    {container.classList.add("hover-right");
+})
